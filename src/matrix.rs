cf1ac084b40850c1604cd70a59fc5ace325c2a92
Andrian Yarotskyi <a.yarotskyi@gmail.com>
2025-06-19 15:07:56 +0200

    move seam extraction logic to matrix implementation

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,4 +1,5 @@
 use crate::*;
+use ::rand::{rngs::ThreadRng, Rng};
 
 #[derive(Clone)]
 pub struct Seam {
@@ -108,8 +109,200 @@ where
     }
 }
 
+impl Matrix<f32> {
+    pub fn extract_vertical_seam(&self, rng: &mut ThreadRng) -> (Seam, f32) {
+        let mut dp_result = self.vector.clone();
+        let width = self.width;
+        let height = self.height();
+
+        // fill in the vector using dynamic programming
+        for i in 1..height {
+            for j in 0..width {
+                dp_result[i * width + j] = dp_result[(i - 1) * width + j]
+                    .min(if j == 0 {
+                        dp_result[(i - 1) * width + j]
+                    } else {
+                        dp_result[(i - 1) * width + (j - 1)]
+                    })
+                    .min(if j == width - 1 {
+                        dp_result[(i - 1) * width + j]
+                    } else {
+                        dp_result[(i - 1) * width + (j + 1)]
+                    })
+                    + dp_result[i * width + j];
+            }
+        }
+
+        let mut indices = vec![0; height];
+
+        // calculate the last element in seam by randomly
+        // selecting one of the minimum points in the last row
+        let mut min_indices = Vec::with_capacity(width);
+        let mut current_min = dp_result[(height - 1) * width];
+        dp_result
+            .iter()
+            .enumerate()
+            .skip((height - 1) * width)
+            .for_each(|(index, value)| {
+                if *value < current_min {
+                    min_indices.truncate(0);
+                    min_indices.push(index);
+                    current_min = *value;
+                }
+                if *value == current_min {
+                    min_indices.push(index);
+                }
+            });
+        indices[height - 1] = min_indices[rng.gen_range(0..min_indices.len())];
+
+        // calculate the rest of the indexes for the seam
+        for i in (0..height - 1).rev() {
+            let index = {
+                let mid_index = indices[i + 1] - width;
+                let left_index = if mid_index == i * width {
+                    mid_index
+                } else {
+                    mid_index - 1
+                };
+
+                let mut min_index = if dp_result[left_index] < dp_result[mid_index] {
+                    left_index
+                } else {
+                    mid_index
+                };
+
+                let right_index = if mid_index == width * (i + 1) - 1 {
+                    mid_index
+                } else {
+                    mid_index + 1
+                };
+
+                min_index = if dp_result[min_index] < dp_result[right_index] {
+                    min_index
+                } else {
+                    right_index
+                };
+
+                min_index
+            };
+            indices[i] = index;
+        }
+
+        (
+            Seam {
+                indices: indices
+                    .iter()
+                    .map(|index| self.original_indices[*index])
+                    .collect(),
+                is_vertical: true,
+            },
+            indices
+                .iter()
+                .map(|index| self.vector[*index])
+                .reduce(|acc, value| acc + value)
+                .unwrap(),
+        )
+    }
+    pub fn extract_horizontal_seam(&self, rng: &mut ThreadRng) -> (Seam, f32) {
+        let mut dp_result = self.vector.clone();
+        let width = self.width;
+        let height = self.vector.len() / width;
+
+        // fill in the vector using dynamic programming
+        for i in 0..height {
+            for j in 1..width {
+                dp_result[i * width + j] = dp_result[i * width + j - 1]
+                    .min(if i == 0 {
+                        dp_result[i * width + j - 1]
+                    } else {
+                        dp_result[(i - 1) * width + j - 1]
+                    })
+                    .min(if i == height - 1 {
+                        dp_result[i * width + j - 1]
+                    } else {
+                        dp_result[(i + 1) * width + j - 1]
+                    })
+                    + dp_result[i * width + j];
+            }
+        }
+
+        let mut indices = vec![0; width];
+
+        // calculate the last element in seam by randomly
+        // selecting one of the minimum points in the last column
+        let mut min_indices = Vec::with_capacity(height);
+        let mut current_min = dp_result[width - 1];
+        dp_result
+            .iter()
+            .enumerate()
+            .skip(width - 1)
+            .step_by(width)
+            .for_each(|(index, value)| {
+                if *value < current_min {
+                    min_indices.truncate(0);
+                    min_indices.push(index);
+                    current_min = *value;
+                }
+                if *value == current_min {
+                    min_indices.push(index);
+                }
+            });
+        indices[width - 1] = min_indices[rng.gen_range(0..min_indices.len())];
+
+        // calculate the rest of the indexes for the seam
+        for i in (0..width - 1).rev() {
+            let index = {
+                let mid_index = indices[i + 1] - 1;
+                let top_index = if mid_index >= width {
+                    mid_index - width
+                } else {
+                    mid_index
+                };
+
+                let mut min_index = if dp_result[top_index] < dp_result[mid_index] {
+                    top_index
+                } else {
+                    mid_index
+                };
+
+                let bottom_index = if mid_index >= self.vector.len() - width {
+                    mid_index
+                } else {
+                    mid_index + width
+                };
+
+                min_index = if dp_result[min_index] < dp_result[bottom_index] {
+                    min_index
+                } else {
+                    bottom_index
+                };
+
+                min_index
+            };
+            indices[i] = index;
+        }
+
+        (
+            Seam {
+                indices: indices
+                    .iter()
+                    .map(|index| self.original_indices[*index])
+                    .collect(),
+                is_vertical: false,
+            },
+            indices
+                .iter()
+                .map(|index| self.vector[*index])
+                .reduce(|acc, value| acc + value)
+                .unwrap(),
+        )
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use ::rand::thread_rng;
+
     use super::*;
 
     impl<T: PartialEq> PartialEq for Matrix<T> {
@@ -341,5 +534,23 @@ mod tests {
                 3,
             ),
         );
+    }
+
+    #[test]
+    fn test_horizontal_seam_extraction() {
+        let mut rng = thread_rng();
+        let energy_matrix: Matrix<f32> =
+            Matrix::new(Vec::from([0.0, 1.0, 3.0, 2.0, 0.0, 1.0, 3.0, 2.0, 0.0]), 3);
+        let (seam, _) = energy_matrix.extract_horizontal_seam(&mut rng);
+        assert_eq!(seam.indices, [0, 4, 8]);
+    }
+
+    #[test]
+    fn test_vertical_seam_extraction() {
+        let mut rng = thread_rng();
+        let energy_matrix: Matrix<f32> =
+            Matrix::new(Vec::from([0.0, 1.0, 3.0, 2.0, 0.0, 1.0, 3.0, 2.0, 0.0]), 3);
+        let (seam, _) = energy_matrix.extract_vertical_seam(&mut rng);
+        assert_eq!(seam.indices, [0, 4, 8]);
     }
 }
--- src/seam_carver.rs
+++ src/seam_carver.rs
@@ -1,5 +1,5 @@
 use crate::*;
-use ::rand::{rngs::ThreadRng, thread_rng, Rng};
+use ::rand::thread_rng;
 use std::thread;
 
 pub fn spawn_seam_carver(
@@ -13,255 +13,54 @@ pub fn spawn_seam_carver(
         .name("seam_carver".to_string())
         .spawn(move || {
             let mut rng = thread_rng();
+            let mut energy_matrix = gradient_magnitude(&grayscale(&image_matrix));
+            let mut carved_image_matrix = image_matrix.clone();
+            let mut window_size = window_size_clone.read().unwrap().clone();
             loop {
-                let window_size = window_size_clone.read().unwrap().clone();
-                let mut energy_matrix = gradient_magnitude(&grayscale(&image_matrix));
-                let mut carved_image_matrix = image_matrix.clone();
-                while match window_size_clone.try_read() {
-                    Ok(next_window_size) => window_size == *next_window_size,
-                    Err(_) => true,
-                } || window_size.width < energy_matrix.width
-                    || window_size.height < energy_matrix.height()
+                match window_size_clone.try_read() {
+                    Ok(next_window_size) => {
+                        if *next_window_size != window_size {
+                            window_size = next_window_size.clone();
+                            energy_matrix = gradient_magnitude(&grayscale(&image_matrix));
+                            carved_image_matrix = image_matrix.clone();
+                        }
+                    }
+                    Err(_) => {}
+                };
+
+                if window_size.width >= energy_matrix.width
+                    && window_size.height >= energy_matrix.height()
                 {
-                    if window_size.width >= energy_matrix.width
-                        && window_size.height >= energy_matrix.height()
-                    {
-                        continue;
-                    }
-                    let lesser_energy_seam: Seam = if window_size.width >= energy_matrix.width {
-                        extract_horizontal_seam(&energy_matrix, &mut rng).0
-                    } else if window_size.height >= energy_matrix.height() {
-                        extract_vertical_seam(&energy_matrix, &mut rng).0
+                    continue;
+                }
+
+                let lesser_energy_seam: Seam = if window_size.width >= energy_matrix.width {
+                    energy_matrix.extract_horizontal_seam(&mut rng).0
+                } else if window_size.height >= energy_matrix.height() {
+                    energy_matrix.extract_vertical_seam(&mut rng).0
+                } else {
+                    let (vertical_seam, vertical_seam_energy) =
+                        energy_matrix.extract_vertical_seam(&mut rng);
+                    let (horizontal_seam, horizontal_seam_energy) =
+                        energy_matrix.extract_horizontal_seam(&mut rng);
+
+                    if vertical_seam_energy < horizontal_seam_energy {
+                        vertical_seam
                     } else {
-                        let (vertical_seam, vertical_seam_energy) =
-                            extract_vertical_seam(&energy_matrix, &mut rng);
-                        let (horizontal_seam, horizontal_seam_energy) =
-                            extract_horizontal_seam(&energy_matrix, &mut rng);
-
-                        if vertical_seam_energy < horizontal_seam_energy {
-                            vertical_seam
-                        } else {
-                            horizontal_seam
-                        }
-                    };
-
-                    carved_image_matrix.carve_seam(&lesser_energy_seam);
-                    energy_matrix = gradient_magnitude(&grayscale(&carved_image_matrix));
-
-                    match displayed_image_clone.try_write() {
-                        Ok(mut display_image_write_lock) => {
-                            *display_image_write_lock = matrix_to_image(&carved_image_matrix);
-                        }
-                        Err(_) => {}
+                        horizontal_seam
                     }
+                };
+
+                carved_image_matrix.carve_seam(&lesser_energy_seam);
+                energy_matrix = gradient_magnitude(&grayscale(&carved_image_matrix));
+
+                match displayed_image_clone.try_write() {
+                    Ok(mut display_image_write_lock) => {
+                        *display_image_write_lock = matrix_to_image(&carved_image_matrix);
+                    }
+                    Err(_) => {}
                 }
             }
         })
         .unwrap();
-}
-
-fn extract_vertical_seam(energy_matrix: &Matrix<f32>, rng: &mut ThreadRng) -> (Seam, f32) {
-    let mut dp_result = energy_matrix.vector.clone();
-    let width = energy_matrix.width;
-    let height = energy_matrix.height();
-
-    // fill in the vector using dynamic programming
-    for i in 1..height {
-        for j in 0..width {
-            dp_result[i * width + j] = dp_result[(i - 1) * width + j]
-                .min(
-                    match (0..j)
-                        .rev()
-                        .find(|k| energy_matrix.vector[(i - 1) * width + *k] != INFINITY)
-                    {
-                        Some(index) => dp_result[(i - 1) * width + index],
-                        None => INFINITY,
-                    },
-                )
-                .min(
-                    match ((j + 1)..width)
-                        .find(|k| energy_matrix.vector[(i - 1) * width + *k] != INFINITY)
-                    {
-                        Some(index) => dp_result[(i - 1) * width + index],
-                        None => INFINITY,
-                    },
-                )
-                + dp_result[i * width + j];
-        }
-    }
-
-    let mut indices = vec![0; height];
-
-    // calculate the last element in seam by randomly
-    // selecting one of the minimum points in the last row
-    let mut min_indices = Vec::with_capacity(width);
-    let mut current_min = dp_result[(height - 1) * width];
-    dp_result
-        .iter()
-        .enumerate()
-        .skip((height - 1) * width)
-        .for_each(|(index, value)| {
-            if *value < current_min {
-                min_indices.truncate(0);
-                min_indices.push(index);
-                current_min = *value;
-            }
-            if *value == current_min {
-                min_indices.push(index);
-            }
-        });
-    indices[height - 1] = min_indices[rng.gen_range(0..min_indices.len())];
-
-    // calculate the rest of the indexes for the seam
-    for i in (0..height - 1).rev() {
-        let index = {
-            let mid_index = indices[i + 1] - width;
-            let left_index = match ((i * width)..mid_index)
-                .rev()
-                .find(|k| energy_matrix.vector[*k] != INFINITY)
-            {
-                Some(index) => index,
-                None => mid_index,
-            };
-            let mut min_index = if dp_result[left_index] < dp_result[mid_index] {
-                left_index
-            } else {
-                mid_index
-            };
-
-            let right_index = match ((mid_index + 1)..(width * (i + 1)))
-                .find(|k| energy_matrix.vector[*k] != INFINITY)
-            {
-                Some(index) => index,
-                None => mid_index,
-            };
-
-            min_index = if dp_result[min_index] < dp_result[right_index] {
-                min_index
-            } else {
-                right_index
-            };
-
-            min_index
-        };
-        indices[i] = index;
-    }
-
-    (
-        Seam {
-            indices: indices
-                .iter()
-                .map(|index| energy_matrix.original_indices[*index])
-                .collect(),
-            is_vertical: true,
-        },
-        indices
-            .iter()
-            .fold(0.0, |acc, index| acc + energy_matrix.vector[*index]),
-    )
-}
-
-fn extract_horizontal_seam(energy_matrix: &Matrix<f32>, rng: &mut ThreadRng) -> (Seam, f32) {
-    let mut dp_result = energy_matrix.vector.clone();
-    let width = energy_matrix.width;
-    let height = energy_matrix.vector.len() / width;
-
-    // fill in the vector using dynamic programming
-    for i in 0..height {
-        for j in 1..width {
-            dp_result[i * width + j] = dp_result[i * width + j - 1]
-                .min(
-                    match (0..i)
-                        .rev()
-                        .find(|k| energy_matrix.vector[k * width + j - 1] != INFINITY)
-                    {
-                        Some(index) => dp_result[index * width + j - 1],
-                        None => INFINITY,
-                    },
-                )
-                .min(
-                    match ((i + 1)..height)
-                        .find(|k| energy_matrix.vector[k * width + j - 1] != INFINITY)
-                    {
-                        Some(index) => dp_result[index * width + j - 1],
-                        None => INFINITY,
-                    },
-                )
-                + dp_result[i * width + j];
-        }
-    }
-
-    let mut indices = vec![0; width];
-
-    // calculate the last element in seam by randomly
-    // selecting one of the minimum points in the last column
-    let mut min_indices = Vec::with_capacity(height);
-    let mut current_min = dp_result[width - 1];
-    dp_result
-        .iter()
-        .enumerate()
-        .skip(width - 1)
-        .step_by(width)
-        .for_each(|(index, value)| {
-            if *value < current_min {
-                min_indices.truncate(0);
-                min_indices.push(index);
-                current_min = *value;
-            }
-            if *value == current_min {
-                min_indices.push(index);
-            }
-        });
-    indices[width - 1] = min_indices[rng.gen_range(0..min_indices.len())];
-
-    // calculate the rest of the indexes for the seam
-    for i in (0..width - 1).rev() {
-        let index = {
-            let mid_index = indices[i + 1] - 1;
-            let top_index = match ((mid_index % width)..mid_index)
-                .step_by(width)
-                .rev()
-                .find(|k| energy_matrix.vector[*k] != INFINITY)
-            {
-                Some(index) => index,
-                None => mid_index,
-            };
-
-            let mut min_index = if dp_result[top_index] < dp_result[mid_index] {
-                top_index
-            } else {
-                mid_index
-            };
-
-            let bottom_index = match ((mid_index + width)..energy_matrix.vector.len())
-                .step_by(width)
-                .find(|k| energy_matrix.vector[*k] != INFINITY)
-            {
-                Some(index) => index,
-                None => mid_index,
-            };
-
-            min_index = if dp_result[min_index] < dp_result[bottom_index] {
-                min_index
-            } else {
-                bottom_index
-            };
-
-            min_index
-        };
-        indices[i] = index;
-    }
-
-    (
-        Seam {
-            indices: indices
-                .iter()
-                .map(|index| energy_matrix.original_indices[*index])
-                .collect(),
-            is_vertical: false,
-        },
-        indices
-            .iter()
-            .fold(0.0, |acc, index| acc + energy_matrix.vector[*index]),
-    )
 }
